6365d6e0da57ed24f77219cc4d8d803395eb31bb
wittybot-avi <wittybot@gmail.com>
2026-01-15 00:01:09 +0530

    feat(api): Add GET /api/flows/registry endpoint
    Introduces a new simulated API endpoint `/api/flows/registry` to return a list of planned MES Pilot flows, backed by the `FLOW_REGISTRY_SEED`. This change is part of the foundational work for the API routing and simulation in V3.4.

--- appConfig.ts
+++ appConfig.ts
@@ -4,4 +4,4 @@
 export const APP_NAME = 'BPM-OS';
 export const APP_VERSION = 'V3.4';
 export const BRANCH_ID = 'V3.4';
-export const PATCH_ID = 'V34-FND-BP-06';
+export const PATCH_ID = 'V34-FND-BP-07';
---
+++ components/flow/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Flow UI Harness Barrel
+ * @foundation V34-FND-BP-07
+ */
+
+export * from "./FlowShell";
+export * from "./FlowStep";
+export * from "./FlowFooter";
